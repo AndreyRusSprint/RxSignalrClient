f52639e59b8f8b227247b9da505270e3abe12920
andreyrussprint <andreyrussprint@gmail.com>
2020-01-22 22:05:02 +0700

    Issue 2: Access token provider docstring (#9)

--- src/main/kotlin/com/andreyrussprint/rxsignalrclient/AccessTokenProvider.kt
+++ src/main/kotlin/com/andreyrussprint/rxsignalrclient/AccessTokenProvider.kt
@@ -4,6 +4,11 @@ import io.reactivex.Single
 import io.reactivex.SingleObserver
 import io.reactivex.disposables.Disposables
 
+/**
+ * Token provider for [RxSignalrClient].
+ *
+ * Allows to change [authToken] without having to reinitialize the SignalR client.
+ */
 class AccessTokenProvider : Single<String>() {
     var authToken: String? = null
 
